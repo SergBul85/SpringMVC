6f914eb4664f55145d30221222c32ddb546b2477
SergBul85 <donetsk55555@gmail.com>
2026-05-30 14:04:09 +0300

    init commit

---
+++ src/main/java/com/hstn/thyleaf/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.hstn.thyleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+    @GetMapping("/processForm")
+    public String processForm() {
+        return "helloworld";
+    }
+
+
+
+}
---
+++ src/test/java/com/hstn/thyleaf/ThyleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hstn.thyleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThyleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
