b45cb6ba1eee48a249d92669cd0a9afac2803000
SergBul85 <donetsk55555@gmail.com>
2026-05-30 14:31:22 +0300

    P_185

--- src/main/java/com/hstn/thyleaf/controller/HelloWorldController.java
+++ src/main/java/com/hstn/thyleaf/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -21,6 +22,13 @@ public class HelloWorldController {
     @GetMapping("/processFormVersionTwo")
     public String letsShoutDude(HttpServletRequest request, Model model) {
         String studentName = request.getParameter("studentName");
+        studentName = studentName.toUpperCase();
+        model.addAttribute("message", "Yo! " + studentName);
+        return "helloworld";
+    }
+
+    @GetMapping("/processFormVersionThree")
+    public String letsShoutDude(@RequestParam String studentName, Model model) {
         studentName = studentName.toUpperCase();
         model.addAttribute("message", "Yo! " + studentName);
         return "helloworld";
